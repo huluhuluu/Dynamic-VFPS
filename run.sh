3f1073475333bda6ea38741545776d2fad02d09c
hulhuluu <hustzjh@hust.edu.cn>
2026-04-10 15:27:38 +0800

    test: update run script

--- run.sh
+++ run.sh
@@ -13,5 +13,12 @@
 # python -u test_gpu.py --encryption paillier --mi-mode static --epochs 100 --selected 8 | tee paillier_static_ep100_lr0.001_bs256_sc8.log
 
 
-python -u test_gpu.py --encryption tenseal --mi-mode static --epochs 100 --dataset cifar-10 --lr 0.01 | tee cifar10_tenseal_static_ep100_lr0.001_bs256_lep1.log
-python -u test_gpu.py --encryption paillier --mi-mode static --epochs 100 --dataset cifar-10 --lr 0.01 | tee cifar10_paillier_static_ep100_lr0.001_bs256_lep1.log
+# python -u test_gpu.py --encryption tenseal --mi-mode static --epochs 100 --dataset cifar-10 --lr 0.003 | tee cifar10_tenseal_static_ep100_lr0.003_bs256_lep1.log
+# python -u test_gpu.py --encryption paillier --mi-mode static --epochs 100 --dataset cifar-10 --lr 0.003 | tee cifar10_paillier_static_ep100_lr0.003_bs256_lep1.log
+
+
+python -u test_gpu.py --encryption tenseal --mi-mode static --epochs 300 --dataset cifar-10 --lr 0.01 | tee cifar10_tenseal_static_ep200_lr0.01_bs256_lep1.log
+python -u test_gpu.py --encryption paillier --mi-mode static --epochs 300 --dataset cifar-10 --lr 0.01 | tee cifar10_paillier_static_ep200_lr0.01_bs256_lep1.log
+
+# python -u test_gpu.py --encryption tenseal --mi-mode static --epochs 200 --dataset cifar-10 --lr 0.01 --local-epochs 3 | tee cifar10_tenseal_static_ep200_lr0.01_bs256_lep3.log
+# python -u test_gpu.py --encryption paillier --mi-mode static --epochs 200 --dataset cifar-10 --lr 0.01 --local-epochs 3 | tee cifar10_paillier_static_ep200_lr0.01_bs256_lep3.log
